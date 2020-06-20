b5589ec5f18da4a337aef96e20812c6dbaf36ae2
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-06-20 18:13:43 -0500

    Feat: added styling css file which is imported by class component Person and managed by App.css.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     //JSX is being returned
     return (
-        <div>
+        <div className={"Person"}>
             <p onClick={props.click}>I'm {props.name} and I am {Math.floor(Math.random() * 100)} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
