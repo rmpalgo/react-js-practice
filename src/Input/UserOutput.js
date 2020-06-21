3bfb5a62462a7c5a1e919925a57e9ca81b40c7a9
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-06-20 19:17:14 -0500

    Feat: imported two output components and rendered.

---
+++ src/Input/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return(
+        <div>
+            <p>Paragraph 1</p>
+            <p>Paragraph 2</p>
+        </div>
+    )
+}
+
+export default userOutput;
