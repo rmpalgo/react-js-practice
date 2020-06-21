18d14b88cde8504c96519077e521be9d9cf8a143
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-06-20 19:28:10 -0500

    feat: used state to pass values to props.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import './App.css';
 
 
 class App extends Component {
+    state = {
+        fruits: [
+            {fruit: "Mango"},
+            {fruit: "Apple"},
+            {fruit: "Banana"}
+        ]
+    }
+
   render() {
 
     return (
         <div className="App">
           <div>
-            <UserOutput fruitOne={"Mango"} fruitTwo={"Apple"} />
+              <h1>This is an example of passing var with props</h1>
+            <UserOutput
+                //passing variables and displaying
+                fruitOne={"Mango"}
+                fruitTwo={"Apple"} />
           </ div>
+            <h1>This is an example of using state and passing it to props</h1>
+            <UserOutput
+                fruitOne={this.state.fruits[0].fruit}
+                fruitTwo={this.state.fruits[1].fruit}
+            />
         </div>
     );
   }
