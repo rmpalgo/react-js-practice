ad6042a9e7a34993c80997a8a6271c69b138f2e6
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-06-23 19:54:34 -0500

    Feat: included styled components dependency and utilized to write vanilla css inline.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
+import styled from 'styled-components';
 import Person from './Person/Person';
 
+const StyledButton = styled.button`
+    background-color: ${props => props.alt ? 'red' : 'green'};
+    color: white;
+    font: inherit;
+    border: 1px solid blue;
+    padding: 8px;
+    cursor: pointer;
+    
+    &:hover {
+        background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+        color: black;
+        }
+`;
 
 
 //class based components
@@ -83,11 +96,11 @@ class App extends Component {
                     })}
                 </div>
             );
-            style.backgroundColor = 'red';
-            style[':hover'] =  {
-                backgroundColor: 'salmon',
-                    color: 'black'
-            }
+            // style.backgroundColor = 'red';
+            // style[':hover'] =  {
+            //     backgroundColor: 'salmon',
+            //         color: 'black'
+            // }
         }
 
         const classes = [];
@@ -100,19 +113,17 @@ class App extends Component {
 
         return (
             //JSX
-            <StyleRoot>
+
                 <div className="App">
                     <div>
                         <h1>React app ES6</h1>
                         <p className={classes.join(' ')}>This is really cool!</p>
-                        <button
-                            style={style}
+                        <StyledButton alt={this.state.showPersons}
                             onClick={this.togglePersonsHandler}
-                            >Switch Button</button>
+                            >Switch Button</StyledButton>
                         {persons}
                     </div>
                 </div>
-            </StyleRoot>
 
         );
         // ^ code in the return gets compiled to code below:
@@ -120,53 +131,4 @@ class App extends Component {
     }
 }
 
-export default Radium(App);
-
-
-
-//hooks equivalent
-/*
-const App = props => {
-    //useState is a Hook since this is a functional component you can't just add state, import useState instead as a Hook to setState
-   const [ personsState, setPersonsState ] = useState({
-        persons: [
-            { name: "Brownie", breed: "chihuaha"},
-            { name: "Cinnamon", breed: "chihuaha"},
-            { name: "Fox Lady", breed: "chihuaha"}
-        ],
-        otherState: 'other value'
-    });
-
-    const [otherState, setOtherState] = useState('some other value');
-    console.log(otherState, setOtherState);
-
-    const switchNameHandler = () => {
-        //console.log('Was clicked!');
-        //Don't access and alter state like this: this.state.persons[0].name = "Milo";
-        setPersonsState({
-            persons: [
-                {name: "Rhean", breed: "Warlock"},
-                {name: "Ruchie", breed: "Warrior"},
-                {name: "Ron", breed: "Templar"}
-            ]
-        });
-    };
-        return (
-            //JSX
-            <div className="App">
-                <div>
-                    <h1>React app ES6</h1>
-                    <p>This is really cool!</p>
-                    <button onClick={switchNameHandler}>Switch Button</button>
-                    <Person name={personsState.persons[0].name} breed={personsState.persons[0].breed}>Hobby is eating snacks!</Person>
-                    <Person name={personsState.persons[1].name} breed={personsState.persons[1].breed}>Hobby is chasing after squirrels.</Person>
-                    <Person name={personsState.persons[2].name} breed={personsState.persons[2].breed}>Hobby is teasing Mia.</Person>
-                </div>
-            </div>
-        );
-        // ^ code in the return gets compiled to code below:
-        // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Is this Rendering on the page?'));
-}
- */
-
-// export default App;
+export default App;
