d21717014995cc228112460d360bbdc9b2762971
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-08 17:31:46 -0500

    exercise: proper import and render an instance of functional component.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>
-    <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-    </ul>
-</div>, document.getElementById("root"))
+function MyInfo() {
+    return (
+        <div>
+            <h1>Ron Palencia</h1>
+            <p>Foodie, Techie, and Funny!</p>
+            <p>3 vacation spots</p>
+            <ol>
+                <li>Tokyo</li>
+                <li>Galapagos Islands</li>
+                <li>Australia</li>
+            </ol>
+        </div>
+    )
+}
 
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
