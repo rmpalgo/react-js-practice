08de23c12aab0f4d93e133442dff149c9c19096d
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-09 17:00:51 -0500

    exercise: inline style jsx.

--- src/index.js
+++ src/index.js
@@ -5,16 +5,25 @@ function App() {
     const date = new Date()
     const hours = date.getHours()
     let timeOfDay = ""
+    const styles = {
+        backgroundColor: "gray",
+        fontSize: "90px"
+    }
 
     if(hours < 12) {
         timeOfDay = "morning"
+        styles.color = "#e3d86d"
     } else if(hours > 12 && hours < 17) {
         timeOfDay = "afternoon"
+        styles.color = "#84b120"
     } else {
         timeOfDay = "evening"
+        styles.color = "#3e0606"
     }
 
-    return (<h1>Good, {`${timeOfDay}!`}</h1>)
+
+
+    return (<h1 style={styles}>Good, {`${timeOfDay}!`}</h1>)
 }
 
 
