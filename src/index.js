6e100684b7967a483d3029c8d671fcd03b9355d2
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-09 15:17:29 -0500

    exercise: simple js logic with jsx.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
+
+function App() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay = ""
+
+    if(hours < 12) {
+        timeOfDay = "morning"
+    } else if(hours > 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "evening"
+    }
+
+    return (<h1>Good, {`${timeOfDay}!`}</h1>)
+}
 
 
-ReactDOM.render(< App/>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
