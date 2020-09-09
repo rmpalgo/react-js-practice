f3125f52d6a07adb70b07a407e78825a21b42eb7
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-08 21:04:36 -0500

    exercise: move functional component to own folder- components.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-function MyInfo() {
-    return (
-        <div>
-            <h1>Ron Palencia</h1>
-            <p>Foodie, Techie, and Funny!</p>
-            <p>3 vacation spots</p>
-            <ol>
-                <li>Tokyo</li>
-                <li>Galapagos Islands</li>
-                <li>Australia</li>
-            </ol>
-        </div>
-    )
-}
+import React from "react"
+import ReactDOM from "react-dom"
+import MyInfo from "./components/MyInfo"
 
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"))
