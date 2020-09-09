0e70de9c0771ea9092843f307ecc9d24195c5401
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-08 21:46:24 -0500

    review: example of an App with sections bundled inside the App component.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,56 @@
+import React from "react"
 
+function App() {
+    return (
+        <div>
+
+            {/*Navbar*/}
+            <nav>
+                <ul className="nav justify-content-center">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Active</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </nav>
+
+            {/*Main*/}
+            <main>
+                <div className="card">
+                    <div className="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+            </main>
+
+            {/*Footer*/}
+            <footer>
+                <ul className="nav justify-content-center">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Active</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </footer>
+        </div>
+    )
+}
+
+
+export default App
