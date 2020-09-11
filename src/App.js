b5ba340b61bac72da79cf176c2c727782d58b7ce
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-11 06:22:31 -0500

    exercise: todo list card.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react"
 import TodoItem from "./components/TodoItem"
+import "./style.css"
 
 function App() {
     return (
-        <div>
+        <div className="card" style={{width: "18rem", margin: "8rem auto", padding: "2em"}}>
             <TodoItem />
             <TodoItem />
             <TodoItem />
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,12 +3,11 @@ import React from "react"
 function TodoItem() {
 
     return(
-        <div>
+
             <div className="form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
                 <label className="form-check-label" htmlFor="exampleCheck1">Item 1</label>
             </div>
-        </div>
     )
 }
 
