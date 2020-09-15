07e6c54c6586963bcda27a6e99804418628e6173
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-15 11:42:20 -0500

    exercise: used ternary operatation to render question element or not.

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,21 +1,24 @@
 import React from "react"
 
 function Joke(props) {
-    let jokeCard =
-        <div>
-        <p>Question: {props.joke.question}</p>
-        <p>Punchline: {props.joke.punchline}</p>
-        </div>
-console.log(props.joke.question)
-    if(props.joke.question === undefined) {
-        jokeCard =
-            <div>
-                <p>Punchline: {props.joke.punchline}</p>
-            </div>
-    }
+//     let jokeCard =
+//         <div>
+//         <p>Question: {props.joke.question}</p>
+//         <p>Punchline: {props.joke.punchline}</p>
+//         </div>
+// console.log(props.joke.question)
+//     if(props.joke.question === undefined) {
+//         jokeCard =
+//             <div>
+//                 <p>Punchline: {props.joke.punchline}</p>
+//             </div>
+//     }
 
     return (
-        jokeCard
+        <div>
+            <h3 style={{display: props.joke.question ? "block" : "none"}}>Question: {props.joke.question}</h3>
+            <h3 style={{color: props.joke.question ? "#000" : "#888"}}>Question: {props.joke.punchline}</h3>
+        </div>
     )
 }
 
