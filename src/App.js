f69ce22baa156cb7e2c817aad35e0b4930003b1b
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-15 13:36:24 -0500

    exercise: react project setup.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from "react"
-import Info from "./components/Info"
-import infoData from "./infoData"
 
 function App() {
-    const infoComponents = infoData.map( info => {
-        return (
-            <Info key={info.id} data={{
-                id: info.id,
-                name: info.name,
-                email: info.email,
-                languages: info.languages,
-                yearsOfExperience: info.yearsOfExperience
-            }} />
-        )
-    })
-
     return (
         <div>
-            {infoComponents}
+            <h1>Hello</h1>
         </div>
     )
 }
-
 
 export default App
---
+++ src/dataProducts.js
@@ -0,0 +1,40 @@
+const products = [
+    {
+        id: "1",
+        name: "Pencil",
+        price: 2.00,
+        description: "Write stuff down."
+    },
+    {
+        id: "2",
+        name: "House",
+        price: 20.00,
+        description: "You can eat and sleep inside the infrastructure. May have ghost inside."
+    },
+    {
+        id: "3",
+        name: "Computer",
+        price: 200.00,
+        description: "Code Code Code."
+    },
+    {
+        id: "4",
+        name: "Coffee",
+        price: 3.00,
+        description: "Very important!"
+    },
+    {
+        id: "5",
+        name: "Snacks",
+        price: 0.00,
+        description: "I only eat free snacks!"
+    },
+    {
+        id: "6",
+        name: "Rubber Duck",
+        price: 5.00,
+        description: "Cody the Duck!"
+    }
+]
+
+export default products;
